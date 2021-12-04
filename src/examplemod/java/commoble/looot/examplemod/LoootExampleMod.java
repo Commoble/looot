65023572773b29bfea91f0657fa9ba570656159d
Commoble <josephbettendorff@yahoo.com>
2021-12-04 10:41:33 -0600

    looot-example-mod => looot_example_mod (module naming rules)

--- src/examplemod/java/commoble/looot/examplemod/LoootExampleMod.java
+++ src/examplemod/java/commoble/looot/examplemod/LoootExampleMod.java
@@ -5,7 +5,7 @@ import net.minecraftforge.fml.common.Mod;
 @Mod(LoootExampleMod.MODID)
 public class LoootExampleMod
 {
-	public static final String MODID = "looot-example-mod";
+	public static final String MODID = "looot_example_mod";
 	
 	public LoootExampleMod()
 	{
